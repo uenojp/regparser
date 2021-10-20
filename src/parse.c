c2211bed92c2fb9e428e37bd13b688a140a92b76
ueno takahiro <uenotakahiro.jp@gmail.com>
2021-10-20 19:22:52 +0900

    refactor

--- src/parse.c
+++ src/parse.c
@@ -1,8 +1,10 @@
 #include "regparser.h"
 
 int parse(const char* sentence) {
-    if (!is_sentence(sentence))
+    if (!is_sentence(sentence)) {
+        fprintf(stderr, "invalid sentence\n");
         return 1;
+    }
 
     /* +1+1 は\0とスペース挿入用 */
     char* copy = (char*)malloc((strlen(sentence) + 1 + 1) * sizeof(char));
@@ -26,11 +28,19 @@ int parse(const char* sentence) {
 
     char* token = strtok(copy, " ");
     pos = transit(pos, token);
-    if (pos == UNKNOWN)
+    if (pos == UNKNOWN) {
+        fprintf(stderr, "invalid rule applied to %s\n", token);
+        free(copy);
         return 1;
+    }
     printf("(%s, %s)\n", token, to_string(pos));
     while ((token = strtok(NULL, " ")) != NULL) {
         pos = transit(pos, token);
+        if (pos == UNKNOWN) {
+            fprintf(stderr, "invalid rule applied to %s\n", token);
+            free(copy);
+            return 1;
+        }
         printf("(%s, %s)\n", to_string(pos), token);
     }
 
@@ -47,17 +57,8 @@ PartOfSpeech transit(const PartOfSpeech state, const char* word) {
     }
 
     PartOfSpeech pos = get_part_of_speech(word);
-    if (pos == UNKNOWN) {
-        fprintf(stderr, "%s is not resisterd in dictionary\n", word);
-        return UNKNOWN;
-    }
 
-    if (has_rule(state, pos)) {
-        return pos;
-    } else {
-        fprintf(stderr, "no rule from %s to %s\n", to_string(state), to_string(pos));
-        return UNKNOWN;
-    }
+    return has_rule(state, pos) ? pos : UNKNOWN;
 }
 
 /* strが文であるか確認する
@@ -65,22 +66,17 @@ PartOfSpeech transit(const PartOfSpeech state, const char* word) {
 bool is_sentence(const char* str) {
     const size_t len = strlen(str);
 
-    if (len == 0 || (len == 1 && str[0] == '.')) {
-        fprintf(stderr, "missing sentence\n");
+    if (len == 0 || (len == 1 && str[0] == '.'))
         return false;
-    }
 
     for (size_t i = 0; i < len; i++) {
         /* アルファベット、スペース、ピリオドから構成されていないとき、strを文として認めない */
-        if (!(isalpha(str[i]) || strchr(" .", str[i]))) {
-            fprintf(stderr, "invalid sentence that contains non-alphabetic characters.\n");
+        if (!(isalpha(str[i]) || strchr(" .", str[i])))
             return false;
-        }
+
         /* ピリオドを丁度1つだけ含み、それが文末にあるとき以外は、strを文として認めない */
-        if (str[i] == '.' && i != len - 1) {
-            fprintf(stderr, "up to 1 sentence\n");
+        if (str[i] == '.' && i != len - 1)
             return false;
-        }
     }
 
     return true;
--- test/test_parse.c
+++ test/test_parse.c
@@ -4,6 +4,7 @@ int main(int argc, char **argv) {
     load_dict("./asset/dictionary");
     load_rule("./asset/rules");
 
+    /* transit */
     assert(transit(START, ".") == END);
     assert(transit(DET, "child") == NOUN);
     assert(transit(NOUN, "to") == PREP);
@@ -12,6 +13,14 @@ int main(int argc, char **argv) {
     assert(transit(ADV, "the") == UNKNOWN);
     assert(transit(VERB, "large") == UNKNOWN);
     assert(transit(START, "large") == UNKNOWN);
+
+    /* is_sentence */
+    assert(is_sentence("Hi there.") == true);
+    assert(is_sentence("Unix never says please.") == true);
+    assert(is_sentence("no period sentence") == true);
+
+    assert(is_sentence("") == false);
+    assert(is_sentence(".") == false);
 
     return 0;
 }
